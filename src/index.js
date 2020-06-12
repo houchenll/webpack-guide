1ee05094aad55f255895e4bec94ed6543e872025
houchenll <houchenll@163.com>
2020-06-12 23:36:06 +0800

    Wrapping up for Output Management

--- src/index.js
+++ src/index.js
@@ -1,29 +1,9 @@
 import _ from 'lodash';
-import './style.css';
-import Wallpaper from './wallpaper.jpg';
-import Data from './data.xml';
-
-// No warning
-import data from './data.json';
-
-// Warning shown, this is not allowed by the spec.
-// import { foo } from './data.json';
 
 function component() {
     const element = document.createElement('div');
 
-    // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    // Add the image to our existing div.
-    const myIcon = new Image();
-    myIcon.src = Wallpaper;
-
-    element.appendChild(myIcon);
-
-    console.log(Data);
-    console.log(data);
 
     return element;
 }
