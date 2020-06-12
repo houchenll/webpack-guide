ce7ca840597c6e45f93f16187f9e5ccf3514c0cc
houchenll <houchenll@163.com>
2020-06-12 23:13:43 +0800

    Loading CSS

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,15 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+    ],
+  },
 };
