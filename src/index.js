c05a5636df500f426f47b6d0042ba6af00ffa99a
houchenll <houchenll@163.com>
2020-06-12 23:20:26 +0800

    Loading Images

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import './style.css';
+import Wallpaper from './wallpaper.jpg';
 
 function component() {
     const element = document.createElement('div');
@@ -7,6 +8,12 @@ function component() {
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+
+    // Add the image to our existing div.
+    const myIcon = new Image();
+    myIcon.src = Wallpaper;
+
+    element.appendChild(myIcon);
 
     return element;
 }
